06744480408369608038a52459fd77a7aa33c229
Administrator <administrator@CN-7.lan>
2019-03-21 17:01:55 +0000

    first commit

--- src/getTrainInfo.js
+++ src/getTrainInfo.js
@@ -2,8 +2,7 @@ const request = require("request");
 const express = require("express");
 
 const getTrainInfoFromStationCode = (station_code, date, time, callback) => {
-    let url = `/uk/train/station/${station_code}/${date}/${time}/timetable.json`;
-  
+    let url = `http://transportapi.com/v3/uk/train/station/${station_code}/${date}/${time}/timetable.json?type=train_station&app_id=06fa4af4&app_key=131158f245f478626a1f8c44a1927eec`;
     request({ url, json: true }, (error, response) => {
       if (error) {
         callback("Unable to connect to train services!", undefined);
