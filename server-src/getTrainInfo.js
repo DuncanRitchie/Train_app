fd83a5782216806d2e24c2ea354d9170976565ca
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-22 18:38:09 +0000

    Wrote for-loops for getTrainInfo.

--- server-src/getTrainInfo.js
+++ server-src/getTrainInfo.js
@@ -8,8 +8,34 @@ const getTrainInfoFromStationCode = (fromStationCode, toStationCode, date, time,
             callback("Unable to connect to train services!", undefined);
         } else if (response.body.departures.all.length === 0) {
             callback("Unable to find train info. Try another search.", undefined);
-        } else {
+        } else if (!toStationCode) {
             callback(undefined, { allDepartures: response.body.departures.all });
+        }
+        else {
+            let wantedDepartures = [];
+            for (i=0;i<response.body.departures.all.length;i++) {
+                let service = response.body.departures.all[i].service;
+                let url = `https://transportapi.com/v3/uk/train/service/${service}///timetable.json?app_id=06fa4af4&app_key=131158f245f478626a1f8c44a1927eec&darwin=false&live=false`;
+                request({url,json:true},(error,res)=>{
+                    if (error) {
+                        console.log(error)
+                        callback("Unable to get information on stations calling at. Try another search.")
+                    }
+                    else {
+                        for (j=0;j<res.body.stops.length;j++) {
+                            if (res.body.stops[j].station_code == toStationCode) {
+                                wantedDepartures.push(response.body.departures.all[i])
+                            }
+                        }
+                        if (wantedDepartures==[]) {
+                            callback(`No services found going from ${fromStationCode} to ${toStationCode}`,undefined)
+                        }
+                        else {
+                            callback(undefined,{allDepartures: wantedDepartures})
+                        }
+                    }
+                })
+            }
         }
     });
 }; 
