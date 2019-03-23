1acef84be23c1aa53775e9ad9135b7e063926f1b
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-23 16:06:17 +0000

    Just commented out all the code I'll come back to.

--- server-src/getTrainInfo.js
+++ server-src/getTrainInfo.js
@@ -1,6 +1,5 @@
 const request = require("request");
-const express = require("express");
-const async = require("async")
+// const async = require("async")
 
 const getTrainInfoFromStationCode = (fromStationCode, toStationCode, date, time, callback) => {
     let url = `http://transportapi.com/v3/uk/train/station/${fromStationCode}/${date}/${time}/timetable.json?app_id=06fa4af4&app_key=131158f245f478626a1f8c44a1927eec`;
@@ -13,40 +12,39 @@ const getTrainInfoFromStationCode = (fromStationCode, toStationCode, date, time,
             callback(undefined, { allDepartures: response.body.departures.all });
         } else {
             let wantedDepartures = [];
-            for (i = 0; i < response.body.departures.all.length; i++) {
-                let service = response.body.departures.all[i].service;
-                let url = `https://transportapi.com/v3/uk/train/service/${service}///timetable.json?app_id=06fa4af4&app_key=131158f245f478626a1f8c44a1927eec&darwin=false&live=false`;
-                request({ url, json: true }, (error, res) => {
-                    if (error) {
-                        console.log(error)
-                        callback("Unable to get information on stations calling at. Try another search.", undefined)
-                    } else {
-                        async.forEachOf(res.body.stops, function(stop, j, callback) {
-                            console.log(`Testing station ${j} - ${stop.station_name}`)
-                            if (stop.station_code == toStationCode) {
-                                console.log("This is the toStation!")
-                                wantedDepartures.push(response.body.departures.all[i])
-                            }
-                        }, function(err) {
-                            if (err) console.error(err.message);
-                            // if (wantedDepartures == []) {
-                            //     callback(`No services found going from ${fromStationCode} to ${toStationCode}`, undefined)
-                            // } else {
-                            console.log(wantedDepartures)
-                            callback(undefined, { allDepartures: wantedDepartures })
-                                // };
-                        });
-                        // for (j = 0; j < res.body.stops.length; j++) {
-                        //     if (res.body.stops[j].station_code == toStationCode) {
-                        //         wantedDepartures.push(response.body.departures.all[i])
-                        //     }
-                        // }
-
-                    }
-                })
-            }
+            // async.filter(response.body.departures.all, doesDepartureGoToToStation(departure, toStationCode, callback), function(err, results) {
+            // });
+            // async.forEachOf(response.body.departures.all, function(departure, i, callback) {
+            //     console.log(`Testing service ${departure.service}`)
+            //     let serviceUrl = `https://transportapi.com/v3/uk/train/service/${departure.service}///timetable.json?app_id=06fa4af4&app_key=131158f245f478626a1f8c44a1927eec&darwin=false&live=false`;
+            //     request({ serviceUrl, json: true }, (error, res) => {
+            //         if (error) {
+            //             console.log(error)
+            //             callback("Unable to get information on stations calling at. Try another search.", undefined)
+            //         } else {
+            //             async.forEachOf(res.body.stops, function(stop, j, callback) {
+            //                 console.log(`Testing station ${j} - ${stop.station_name}`)
+            //                 if (stop.station_code == toStationCode) {
+            //                     console.log("This is the toStation!")
+            //                     wantedDepartures.push(response.body.departures.all[i])
+            //                 }
+            //             });
+            //         }
+            //     })
+            // }, function(err) {
+            //     if (err) console.error(err.message);
+            // if (wantedDepartures == []) {
+            //     callback(`No services found going from ${fromStationCode} to ${toStationCode}`, undefined)
+            // } else {
+            console.log(wantedDepartures)
+            callback(undefined, { allDepartures: wantedDepartures })
+                //      };
         }
-    });
-};
+    })
+}
+
+const doesDepartureGoToToStation = (departure, toStationCode, callback) => {
+
+}
 
 module.exports = getTrainInfoFromStationCode
