3cc880f7ee2db37cb6b07952a679cbb653a2236b
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-23 18:07:26 +0000

    Still not working. Will try async again.

--- server-src/getTrainInfo.js
+++ server-src/getTrainInfo.js
@@ -8,24 +8,25 @@ const getTrainInfoFromStationCode = async function(fromStationCode, toStationCod
             callback("Unable to connect to train services!", undefined);
         } else if (response.body.departures.all.length === 0) {
             callback("Unable to find train info. Try another search.", undefined);
-        } else if (!toStationCode) {
+        } else if (toStationCode === undefined) {
             callback(undefined, { allDepartures: response.body.departures.all });
         } else {
             let wantedDepartures = [];
             for (i = 0; i < response.body.departures.all.length; i++) {
+                let currentDeparture = response.body.departures.all[i];
                 let service = response.body.departures.all[i].service;
                 console.log("Checking service " + service)
                 let url = `https://transportapi.com/v3/uk/train/service/${service}///timetable.json?app_id=06fa4af4&app_key=131158f245f478626a1f8c44a1927eec&darwin=false&live=false`;
                 request({ url, json: true }, (error, res) => {
                     if (error) {
                         console.log(error)
-                        callback("Unable to get information on stations calling at. Try another search.")
+                        callback("Unable to get information on stations calling at. Try another search.", undefined)
                     } else {
                         for (j = 0; j < res.body.stops.length; j++) {
                             console.log(`Checking station ${j} - ${res.body.stops[j].station_name}`)
                             if (res.body.stops[j].station_code == toStationCode) {
                                 console.log("This is the toStation!")
-                                wantedDepartures.push(response.body.departures.all[i])
+                                wantedDepartures.push(currentDeparture)
                             }
                         }
                         if (wantedDepartures == []) {
