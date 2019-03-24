749fea9ce63e3abb4452bcbbac3ad96b9af5e6ba
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-24 14:51:10 +0000

    Replaced all old app_id and app_key with new.

--- server-src/getTrainInfo.js
+++ server-src/getTrainInfo.js
@@ -2,31 +2,31 @@ const request = require("request");
 
 const getTrainInfoFromStationCode = async function(fromStationCode, toStationName, date, time, callback) {
     let url = `http://transportapi.com/v3/uk/train/station/${fromStationCode}/${date}/${time}/timetable.json?app_id=fea7751f&app_key=702c5fbff7c11ddaa834da79ac4e6ddf`;
-    //let url = `http://transportapi.com/v3/uk/train/station/${fromStationCode}/live.json?app_id=06fa4af4&app_key=131158f245f478626a1f8c44a1927eec`
+    //let url = `http://transportapi.com/v3/uk/train/station/${fromStationCode}/live.json?app_id=fea7751f&app_key=702c5fbff7c11ddaa834da79ac4e6ddf`
     console.log(url)
-    request({ url, json: true },(error, response) => {
+    request({ url, json: true }, (error, response) => {
         // console.log(response.body)
         let outboundResult = response.body.departures.all.filter((station) => {
-            //  console.log(station.destination_name)
-            return station.destination_name === toStationName
-        })
-        // console.log(outboundResult)
-        callback(undefined, outboundResult ) 
-        // if (error) {
-        //     callback("Unable to connect to train services!", undefined);
-        // } else if (response.body.departures.all.length === 0) {
-        //     callback("Unable to find train info. Try another search.", undefined);
+                //  console.log(station.destination_name)
+                return station.destination_name === toStationName
+            })
+            // console.log(outboundResult)
+        callback(undefined, outboundResult)
+            // if (error) {
+            //     callback("Unable to connect to train services!", undefined);
+            // } else if (response.body.departures.all.length === 0) {
+            //     callback("Unable to find train info. Try another search.", undefined);
             // } else if (toStationCode === undefined) {
-        //  } else {
+            //  } else {
             // let stations =response.body.departures.all.map((station) => {
             //     console.log(station.destination_name)
-                // return station.destination_name === "London Euston"
+            // return station.destination_name === "London Euston"
             // })
-            
-            // callback(undefined, { allDepartures: response.body.departures.all });
+
+        // callback(undefined, { allDepartures: response.body.departures.all });
         //  }
-         //else 
-         //{
+        //else 
+        //{
         //     console.log('departures')
         //     console.log(response.body.departures)
         //     console.log('departures.all')
@@ -36,7 +36,7 @@ const getTrainInfoFromStationCode = async function(fromStationCode, toStationNam
         //         let currentDeparture = response.body.departures.all[i];
         //         let service = response.body.departures.all[i].service;
         //         console.log("Checking service " + service)
-        //         let url = `https://transportapi.com/v3/uk/train/service/${service}///timetable.json?app_id=06fa4af4&app_key=131158f245f478626a1f8c44a1927eec&darwin=false&live=false`;
+        //         let url = `https://transportapi.com/v3/uk/train/service/${service}///timetable.json?app_id=fea7751f&app_key=702c5fbff7c11ddaa834da79ac4e6ddf&darwin=false&live=false`;
         //         request({ url, json: true }, (error, res) => {
         //             if (error) {
         //                 console.log(error)
@@ -59,9 +59,9 @@ const getTrainInfoFromStationCode = async function(fromStationCode, toStationNam
         //             }
         //         })
         //     } 
-    //     }
-    // })
-})
+        //     }
+        // })
+    })
 }
 
 module.exports = getTrainInfoFromStationCode
