6bbaf37d15b30cd22b025a01a358f864ce77b997
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-24 16:28:00 +0000

    Wrote handlePageDisplayed() to set the state.

--- client/src/App.js
+++ client/src/App.js
@@ -6,129 +6,141 @@ import ResultPage from './components/home/ResultPage';
 import NewsPage from './components/service-news/NewsPage';
 import StationPage from './components/stationinfo/StationPage';
 import './App.css';
-import {debounce} from 'debounce';
+import { debounce } from 'debounce';
 import AwesomeDebouncePromise from 'awesome-debounce-promise';
 
-const searchFromAPI = (fromStationParam) => fetch ('http://localhost:3001/chooseFromStation?fromStation=' + fromStationParam)
-const searchToAPI = (toStationParam) => fetch ('http://localhost:3001/chooseToStation?toStation=' + toStationParam)
+const searchFromAPI = (fromStationParam) => fetch('http://localhost:3001/chooseFromStation?fromStation=' + fromStationParam)
+const searchToAPI = (toStationParam) => fetch('http://localhost:3001/chooseToStation?toStation=' + toStationParam)
 
 const searchAPIDebounced = AwesomeDebouncePromise(searchFromAPI, 3000);
 const searchToAPIDebounced = AwesomeDebouncePromise(searchToAPI, 3000);
 
 class App extends Component {
     state = {
-      fromStation:'', 
-      toStation:'', 
-      leavingDate:'', 
-      departingStatus:'',
-      leavingTime:'', 
-      returnCheck:false, 
-      returningDate:'', 
-      returningStatus:'',
-      returningTime:'', 
-      adultCount:1, 
-      childCount:0,
-      outbound:[],
-      chooseFromStations: [],
-      chosenFromStation: '',
-      chosenToStation: '',
-      chooseToStations: [],
-    }
-  
-    handleChange = ({target}) => {
-      const name = target.name
-      const value = target.type === 'checkbox' ? target.checked : target.value
-      this.setState({
-        [name]: value
-      })
+        fromStation: '',
+        toStation: '',
+        leavingDate: '',
+        departingStatus: '',
+        leavingTime: '',
+        returnCheck: false,
+        returningDate: '',
+        returningStatus: '',
+        returningTime: '',
+        adultCount: 1,
+        childCount: 0,
+        outbound: [],
+        chooseFromStations: [],
+        chosenFromStation: '',
+        chosenToStation: '',
+        chooseToStations: [],
+        pageDisplayed: "home"
     }
 
-    asyncFromStation = async () => {
+    handleChange = ({ target }) => {
+        const name = target.name
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        this.setState({
+            [name]: value
+        })
+    }
+
+    asyncFromStation = async() => {
         if (this.state.fromStation) {
-          const {stations} =  await searchAPIDebounced(this.state.fromStation)
-          .then((response) => response.json())
-          this.setState({chooseFromStations: stations})
+            const { stations } = await searchAPIDebounced(this.state.fromStation)
+                .then((response) => response.json())
+            this.setState({ chooseFromStations: stations })
         }
     }
 
     handleChangeFromStation = (e) => {
-      this.setState({
-        fromStation: e.target.value
-      }, this.asyncFromStation )
-      
-    }
-    
-    handleSelectFromStation = (e) =>{
-      this.setState({
-        chosenFromStation: e.target.value
-      })
+        this.setState({
+            fromStation: e.target.value
+        }, this.asyncFromStation)
+
     }
 
-    handleSelectToStation = (e) =>{
-      this.setState({
-        chosenToStation: e.target.value
-      })
+    handleSelectFromStation = (e) => {
+        this.setState({
+            chosenFromStation: e.target.value
+        })
     }
 
-    asyncToStation = async () => {
-      if (this.state.toStation) {
-        const {stations} =  await searchToAPIDebounced(this.state.toStation)
-        .then((response) => response.json())
-        this.setState({chooseToStations: stations})
-      } 
+    handleSelectToStation = (e) => {
+        this.setState({
+            chosenToStation: e.target.value
+        })
+    }
+
+    asyncToStation = async() => {
+        if (this.state.toStation) {
+            const { stations } = await searchToAPIDebounced(this.state.toStation)
+                .then((response) => response.json())
+            this.setState({ chooseToStations: stations })
+        }
     }
 
     handleChangeToStation = (e) => {
-      this.setState({
-        toStation: e.target.value
-      }, this.asyncToStation)
+        this.setState({
+            toStation: e.target.value
+        }, this.asyncToStation)
     }
 
     handleSubmit = (e) => {
-      console.log('submited')
-      e.preventDefault()
-      fetch('http://localhost:3001/train?chosenFromStation=' + this.state.chosenFromStation + '&chosenToStation=' + this.state.chosenToStation + '&leavingDate=' + this.state.leavingDate + '&leavingTime=' + this.state.leavingTime)
-        .then((response) => console.log(response))
-        .then((outbound) => ({outbound}))
+        console.log('submited')
+        e.preventDefault()
+        fetch('http://localhost:3001/train?chosenFromStation=' + this.state.chosenFromStation + '&chosenToStation=' + this.state.chosenToStation + '&leavingDate=' + this.state.leavingDate + '&leavingTime=' + this.state.leavingTime)
+            .then((response) => console.log(response))
+            .then((outbound) => ({ outbound }))
     }
 
-  render() {
-    
-    const { chooseToStations, chooseFromStations, fromStation, toStation, leavingDate, departingStatus, leavingTime, returnCheck, returningDate, returningStatus, returningTime, adultCount, childCount } = this.state
-    return (
-      <div className="App">
-        <HeaderBar title="live times & tickets"/>
-        <Home 
-          fromStation={fromStation}
-          toStation={toStation} 
-          leavingDate={leavingDate}
-          departingStatus={departingStatus}
-          leavingTime={leavingTime}
-          returnCheck={returnCheck} 
-          returningDate={returningDate}
-          returningStatus={returningStatus}
-          returningTime={returningTime}
-          adultCount={adultCount}
-          childCount={childCount}
-          chooseFromStations={chooseFromStations}
-          chooseToStations={chooseToStations}
-          handleSelectFromStation={this.handleSelectFromStation}
-          handleSelectToStation={this.handleSelectToStation}
-          handleChange = {this.handleChange}
-          handleChangeFromStation = {this.handleChangeFromStation}
-          handleChangeToStation = {this.handleChangeToStation}
-          handleSubmit = {this.handleSubmit}
-        />
+    handlePageDisplayed = (page) => {
+        this.setState({ pageDisplayed: page })
+    }
 
-        <ResultPage/>
-        <NewsPage/>
-        <StationPage/>
+    render() {
 
-        <MenuBar/>
-          
-      </div>
-    );
-  }
+        const { chooseToStations, chooseFromStations, fromStation, toStation, leavingDate, departingStatus, leavingTime, returnCheck, returningDate, returningStatus, returningTime, adultCount, childCount } = this.state
+        return ( <
+            div className = "App" >
+            <
+            HeaderBar title = "live times & tickets" / >
+            <
+            Home fromStation = { fromStation }
+            toStation = { toStation }
+            leavingDate = { leavingDate }
+            departingStatus = { departingStatus }
+            leavingTime = { leavingTime }
+            returnCheck = { returnCheck }
+            returningDate = { returningDate }
+            returningStatus = { returningStatus }
+            returningTime = { returningTime }
+            adultCount = { adultCount }
+            childCount = { childCount }
+            chooseFromStations = { chooseFromStations }
+            chooseToStations = { chooseToStations }
+            handleSelectFromStation = { this.handleSelectFromStation }
+            handleSelectToStation = { this.handleSelectToStation }
+            handleChange = { this.handleChange }
+            handleChangeFromStation = { this.handleChangeFromStation }
+            handleChangeToStation = { this.handleChangeToStation }
+            handleSubmit = { this.handleSubmit }
+            />
+
+            <
+            ResultPage / >
+            <
+            NewsPage / >
+            <
+            StationPage / >
+
+            <
+            MenuBar handlePageDisplayed = { this.handlePageDisplayed }
+            / >
+
+            <
+            /div>
+        );
+    }
 }
 
 export default App;
