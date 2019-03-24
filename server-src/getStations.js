81abf01a4602d20cfeb111d852c754088e924c07
Cristien Natal <crisbnp@icloud.com>
2019-03-24 23:18:51 +0000

    API updated

--- server-src/getStations.js
+++ server-src/getStations.js
@@ -2,7 +2,7 @@ const request = require("request")
 const express = require("express");
 
 const getStationsFromSearch = (query, callback) => {
-    let url = `http://transportapi.com/v3/uk/places.json?query=${query}&type=train_station&app_id=06fa4af4&app_key=131158f245f478626a1f8c44a1927eec`;
+    let url = `http://transportapi.com/v3/uk/places.json?query=${query}&type=train_station&app_id=fea7751f&app_key=702c5fbff7c11ddaa834da79ac4e6ddf`;
     request({ url, json: true }, (error, response) => {
         if (error) {
             callback("Unable to connect to train services!", undefined);
