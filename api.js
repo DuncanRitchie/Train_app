fad4cbc4d22b8a384e36d7f9fc998b631ba9ddae
Cristien Natal <crisbnp@icloud.com>
2019-03-25 05:12:20 +0000

    tyring to deploy

--- api.js
+++ api.js
@@ -14,9 +14,13 @@ const getNewsFromStationCode = require("./server-src/getNews")
 const port = process.env.PORT || 3001;
 
 const publicDirectory = path.join(__dirname, "client/build");
-app.use(express.static(publicDirectory));
+app.use(express.static('dist'));
 app.use(cors())
 dotenv.config()
+
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.get("/train", (req, res) => {
 
