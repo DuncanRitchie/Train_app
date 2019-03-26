5686f78fe4c1689b3da24e7d5074bcc93f118276
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-26 13:52:09 +0000

    Made the menu bar not link to the current page.

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -4,9 +4,27 @@ import './MenuBar.css'
 const MenuBar = (props) => {
     return (
         <ul className="nav-bar">
-            <li><a className={props.pageDisplayed === "news" ? "nav-link current-page" : "nav-link"} href="/news">news</a></li>
-            <li><a className={props.pageDisplayed === "home" ? "nav-link current-page" : "nav-link"} href="/">home</a></li>
-            <li><a className={props.pageDisplayed === "station" ? "nav-link current-page" : "nav-link"} href="/station">station</a></li>
+            <li>
+                {
+                    props.pageDisplayed === "news"
+                    ? <a className="nav-link current-page">news</a>
+                    : <a className="nav-link" href="/news">news</a>
+                }
+            </li>
+            <li>
+                {
+                    props.pageDisplayed === "home"
+                    ? <a className="nav-link current-page">home</a>
+                    : <a className="nav-link" href="/">home</a>
+                }
+            </li>
+            <li>
+                {
+                    props.pageDisplayed === "station"
+                    ? <a className="nav-link current-page">station</a>
+                    : <a className="nav-link" href="/station">station</a>
+                }
+            </li>
         </ul>
     )
 }
