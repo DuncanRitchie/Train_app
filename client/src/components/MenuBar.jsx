f3b523639fcc1520d395750bfd12c5fc727ab8ae
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-26 13:59:34 +0000

    Changed the menu bar to a map() across an array of pages.

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -4,27 +4,17 @@ import './MenuBar.css'
 const MenuBar = (props) => {
     return (
         <ul className="nav-bar">
-            <li>
-                {
-                    props.pageDisplayed === "news"
-                    ? <a className="nav-link current-page">news</a>
-                    : <a className="nav-link" href="/news">news</a>
-                }
-            </li>
-            <li>
-                {
-                    props.pageDisplayed === "home"
-                    ? <a className="nav-link current-page">home</a>
-                    : <a className="nav-link" href="/">home</a>
-                }
-            </li>
-            <li>
-                {
-                    props.pageDisplayed === "station"
-                    ? <a className="nav-link current-page">station</a>
-                    : <a className="nav-link" href="/station">station</a>
-                }
-            </li>
+            {["news","home","station"].map((page) => {
+                return (
+                    <li>
+                        {
+                            props.pageDisplayed === page
+                            ? <a className="nav-link current-page">{page}</a>
+                            : <a className="nav-link" href={"/"+page}>{page}</a>
+                        }
+                    </li>
+                )
+            })}
         </ul>
     )
 }
