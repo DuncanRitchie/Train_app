c1c42bf4c12ea62864b828a035a4f6d59a3f2a66
Cristien Natal <crisbnp@icloud.com>
2019-03-26 14:57:01 +0000

    error msg

--- client/src/App.js
+++ client/src/App.js
@@ -143,6 +143,7 @@ class App extends Component {
             chosenToStation: '',
             chooseToStations: [],
             outbound: [],
+            errorMsg: "",
             pageDisplayed: "home",   
         })
     }
